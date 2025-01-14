c17927eacd8ebc6f7f4d2eb2605f32b52d173ba2
LeandroSilva <leandro.silva@virtual-identity.com>
2025-01-14 13:15:06 +0100

    feat: TSYSTEMS-196 Fixing kubernetes pod issues

--- src/main/java/de/caritas/cob/statisticsservice/StatisticsServiceApplication.java
+++ src/main/java/de/caritas/cob/statisticsservice/StatisticsServiceApplication.java
@@ -3,10 +3,8 @@ package de.caritas.cob.statisticsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableMongoRepositories(basePackages = "de.caritas.cob.statisticsservice.api.statistics.repository")
 public class StatisticsServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(StatisticsServiceApplication.class, args);
